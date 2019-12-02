051c53412b69c182baa66e20f7b459129358c685
RyanBurgert219 <sqrtryan@gmail.com>
2019-12-01 19:47:31 -0500

    This is an automatic commit message

--- publish.sh
+++ publish.sh
@@ -2,3 +2,7 @@ sh /Users/Ryan/PycharmProjects/QuickPython/publish.sh
 #Yes, do this twice...
 sudo /usr/local/opt/python/libexec/bin/python -m pip install rp --upgrade
 sudo /usr/local/opt/python/libexec/bin/python -m pip install rp --upgrade
+git add --all
+git commit -am "This is an automatic commit message"
+git push https://github.com/RyannDaGreat/rp master
+
