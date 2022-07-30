555803771c8d049e07a6d0d6b6a32823bca9520f
RyanBurgert219 <sqrtryan@gmail.com>
2022-07-30 19:20:26 -0400

    Succesfully added parso into the git repo (was previously a submodule and so I couldn't track files in it)

---
+++ libs/parso/pgen2/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2004-2005 Elemental Security, Inc. All Rights Reserved.
+# Licensed to PSF under a Contributor Agreement.
+
+# Modifications:
+# Copyright 2006 Google, Inc. All Rights Reserved.
+# Licensed to PSF under a Contributor Agreement.
+# Copyright 2014 David Halter and Contributors
+# Modifications are dual-licensed: MIT and PSF.
+
+from rp.libs.parso.pgen2.generator import generate_grammar
