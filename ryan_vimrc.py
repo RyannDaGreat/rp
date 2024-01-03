adc4db0a6aa5d18a1260b689ec8dc2fe02336cc5
Ryan Burgert (Retina) <ryancentralorg@gmail.com>
2024-01-03 05:06:33 -0500

    Vimrc: Commented out whichkey; it made jedi \j not work

--- ryan_vimrc.py
+++ ryan_vimrc.py
@@ -838,95 +838,95 @@ Plugin 'simeji/winresizer' "Use control+e to resize windows
 
         " Plugin 'RyannDaGreat/vim-signature' "This takes a non-trivial time to start (not bad, but not super fast either). I don't really use marks - this plugin's purpose is to preview where marks are in the gutter.
 
-        "WHICHKEY - Try pressing \<space>l then wait for a second - a menu should appear. This is experimental and I'll use it to make my config easier to use!
-            Plugin 'liuchengxu/vim-which-key'
-
-            " Define prefix dictionary
-            let g:which_key_map =  {}
-
-            let g:mapleader = "\<Space>"
-            let g:maplocalleader = ','
-            nnoremap <silent> <leader>      :<c-u>WhichKey '<Space>'<CR>
-            nnoremap <silent> <localleader> :<c-u>WhichKey  ','<CR>
-
-            nnoremap <leader>1 :1wincmd w<CR>
-            let g:which_key_map.1 = 'which_key_ignore'
-
-            nnoremap <leader>_a :echom '_a'<CR>
-            nnoremap <leader>_b :echom '_b'<CR>
-            let g:which_key_map['_'] = { 'name': 'which_key_ignore' }
-
-            nnoremap <silent> <leader> :<c-u>WhichKey '<Space>'<CR>
-            vnoremap <silent> <leader> :<c-u>WhichKeyVisual '<Space>'<CR>
-
-            call which_key#register('<Space>', "g:which_key_map")
-
-            call which_key#register('<Space>', "g:which_key_map", 'n')
-            call which_key#register('<Space>', "g:which_key_map_visual", 'v')
-
-            " Second level dictionaries:
-            " 'name' is a special field. It will define the name of the group, e.g., leader-f is the "+file" group.
-            " Unnamed groups will show a default empty string.
-
-            " =======================================================
-            " Create menus based on existing mappings
-            " =======================================================
-            " You can pass a descriptive text to an existing mapping.
-
-            let g:which_key_map.f = { 'name' : '+file' }
-
-            nnoremap <silent> <leader>fs :update<CR>
-            let g:which_key_map.f.s = 'save-file'
-
-            nnoremap <silent> <leader>fd :e $MYVIMRC<CR>
-            let g:which_key_map.f.d = 'open-vimrc'
-
-            nnoremap <silent> <leader>oq  :copen<CR>
-            nnoremap <silent> <leader>ol  :lopen<CR>
-            let g:which_key_map.o = {
-                  \ 'name' : '+open',
-                  \ 'q' : 'open-quickfix'    ,
-                  \ 'l' : 'open-locationlist',
-                  \ }
-
-            " =======================================================
-            " Create menus not based on existing mappings:
-            " =======================================================
-            " Provide commands(ex-command, <Plug>/<C-W>/<C-d> mapping, etc.)
-            " and descriptions for the existing mappings.
-            "
-            " Note:
-            " Some complicated ex-cmd may not work as expected since they'll be
-            " feed into `feedkeys()`, in which case you have to define a decicated
-            " Command or function wrapper to make it work with vim-which-key.
-            " Ref issue #126, #133 etc.
-            let g:which_key_map.b = {
-                  \ 'name' : '+buffer' ,
-                  \ '1' : ['b1'        , 'buffer 1']        ,
-                  \ '2' : ['b2'        , 'buffer 2']        ,
-                  \ 'd' : ['bd'        , 'delete-buffer']   ,
-                  \ 'f' : ['bfirst'    , 'first-buffer']    ,
-                  \ 'h' : ['Startify'  , 'home-buffer']     ,
-                  \ 'l' : ['blast'     , 'last-buffer']     ,
-                  \ 'n' : ['bnext'     , 'next-buffer']     ,
-                  \ 'p' : ['bprevious' , 'previous-buffer'] ,
-                  \ '?' : ['Buffers'   , 'fzf-buffer']      ,
-                  \ }
-
-            let g:which_key_map.l = {
-                  \ 'name' : '+lsp',
-                  \ 'f' : ['spacevim#lang#util#Format()'          , 'formatting']       ,
-                  \ 'r' : ['spacevim#lang#util#FindReferences()'  , 'references']       ,
-                  \ 'R' : ['spacevim#lang#util#Rename()'          , 'rename']           ,
-                  \ 's' : ['spacevim#lang#util#DocumentSymbol()'  , 'document-symbol']  ,
-                  \ 'S' : ['spacevim#lang#util#WorkspaceSymbol()' , 'workspace-symbol'] ,
-                  \ 'g' : {
-                    \ 'name': '+goto',
-                    \ 'd' : ['spacevim#lang#util#Definition()'     , 'definition']      ,
-                    \ 't' : ['spacevim#lang#util#TypeDefinition()' , 'type-definition'] ,
-                    \ 'i' : ['spacevim#lang#util#Implementation()' , 'implementation']  ,
-                    \ },
-                  \ }
+        ""WHICHKEY - Try pressing \<space>l then wait for a second - a menu should appear. This is experimental and I'll use it to make my config easier to use!
+        "    Plugin 'liuchengxu/vim-which-key'
+        "
+        "    " Define prefix dictionary
+        "    let g:which_key_map =  {}
+        "
+        "    let g:mapleader = "\<Space>"
+        "    let g:maplocalleader = ','
+        "    nnoremap <silent> <leader>      :<c-u>WhichKey '<Space>'<CR>
+        "    nnoremap <silent> <localleader> :<c-u>WhichKey  ','<CR>
+        "
+        "    nnoremap <leader>1 :1wincmd w<CR>
+        "    let g:which_key_map.1 = 'which_key_ignore'
+        "
+        "    nnoremap <leader>_a :echom '_a'<CR>
+        "    nnoremap <leader>_b :echom '_b'<CR>
+        "    let g:which_key_map['_'] = { 'name': 'which_key_ignore' }
+        "
+        "    nnoremap <silent> <leader> :<c-u>WhichKey '<Space>'<CR>
+        "    vnoremap <silent> <leader> :<c-u>WhichKeyVisual '<Space>'<CR>
+        "
+        "    call which_key#register('<Space>', "g:which_key_map")
+        "
+        "    call which_key#register('<Space>', "g:which_key_map", 'n')
+        "    call which_key#register('<Space>', "g:which_key_map_visual", 'v')
+        "
+        "    " Second level dictionaries:
+        "    " 'name' is a special field. It will define the name of the group, e.g., leader-f is the "+file" group.
+        "    " Unnamed groups will show a default empty string.
+        "
+        "    " =======================================================
+        "    " Create menus based on existing mappings
+        "    " =======================================================
+        "    " You can pass a descriptive text to an existing mapping.
+        "
+        "    let g:which_key_map.f = { 'name' : '+file' }
+        "
+        "    nnoremap <silent> <leader>fs :update<CR>
+        "    let g:which_key_map.f.s = 'save-file'
+        "
+        "    nnoremap <silent> <leader>fd :e $MYVIMRC<CR>
+        "    let g:which_key_map.f.d = 'open-vimrc'
+        "
+        "    nnoremap <silent> <leader>oq  :copen<CR>
+        "    nnoremap <silent> <leader>ol  :lopen<CR>
+        "    let g:which_key_map.o = {
+        "          \ 'name' : '+open',
+        "          \ 'q' : 'open-quickfix'    ,
+        "          \ 'l' : 'open-locationlist',
+        "          \ }
+        "
+        "    " =======================================================
+        "    " Create menus not based on existing mappings:
+        "    " =======================================================
+        "    " Provide commands(ex-command, <Plug>/<C-W>/<C-d> mapping, etc.)
+        "    " and descriptions for the existing mappings.
+        "    "
+        "    " Note:
+        "    " Some complicated ex-cmd may not work as expected since they'll be
+        "    " feed into `feedkeys()`, in which case you have to define a decicated
+        "    " Command or function wrapper to make it work with vim-which-key.
+        "    " Ref issue #126, #133 etc.
+        "    let g:which_key_map.b = {
+        "          \ 'name' : '+buffer' ,
+        "          \ '1' : ['b1'        , 'buffer 1']        ,
+        "          \ '2' : ['b2'        , 'buffer 2']        ,
+        "          \ 'd' : ['bd'        , 'delete-buffer']   ,
+        "          \ 'f' : ['bfirst'    , 'first-buffer']    ,
+        "          \ 'h' : ['Startify'  , 'home-buffer']     ,
+        "          \ 'l' : ['blast'     , 'last-buffer']     ,
+        "          \ 'n' : ['bnext'     , 'next-buffer']     ,
+        "          \ 'p' : ['bprevious' , 'previous-buffer'] ,
+        "          \ '?' : ['Buffers'   , 'fzf-buffer']      ,
+        "          \ }
+        "
+        "    let g:which_key_map.l = {
+        "          \ 'name' : '+lsp',
+        "          \ 'f' : ['spacevim#lang#util#Format()'          , 'formatting']       ,
+        "          \ 'r' : ['spacevim#lang#util#FindReferences()'  , 'references']       ,
+        "          \ 'R' : ['spacevim#lang#util#Rename()'          , 'rename']           ,
+        "          \ 's' : ['spacevim#lang#util#DocumentSymbol()'  , 'document-symbol']  ,
+        "          \ 'S' : ['spacevim#lang#util#WorkspaceSymbol()' , 'workspace-symbol'] ,
+        "          \ 'g' : {
+        "            \ 'name': '+goto',
+        "            \ 'd' : ['spacevim#lang#util#Definition()'     , 'definition']      ,
+        "            \ 't' : ['spacevim#lang#util#TypeDefinition()' , 'type-definition'] ,
+        "            \ 'i' : ['spacevim#lang#util#Implementation()' , 'implementation']  ,
+        "            \ },
+        "            \ }
 
 
 
