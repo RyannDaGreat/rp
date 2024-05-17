00adf336a6348d3fe74ef26fb9b5123dd7399ac3
Ryan Burgert (Retina) <ryancentralorg@gmail.com>
2024-05-16 17:28:30 -0700

    Cleaned up debug_comment

--- experimental/debug_comment.py
+++ experimental/debug_comment.py
@@ -1,6 +1,5 @@
 import inspect
 import atexit
-import re
 import rp
 
 #TODO: Integrate this into RP's repl too somehow
@@ -10,6 +9,7 @@ import rp
 #      Should be able to make it verbose or silent
 #      Perhaps ask a yes/no at the end of whether we shoudl replace
 #      Inform the user of what files have changed at least...ONLY IF they changed...
+#      Allow a frames_back or similar option to debug_comment so other functions can build upon it, adding defaults etc.
 
 def is_zero_arg_callable(func):
     if callable(func):
@@ -21,8 +21,56 @@ def is_zero_arg_callable(func):
 _debug_eval_results = []
 
 def debug_comment(value):
-    # TODO: allow args and kwargs here too, nicely formatted of course
+    """
+    Modifies the source code of the file this was called in!
+    Best practice is to commit all changed to git before this is called so you can revert changes.
 
+    Notes:
+        Because it simply adds or modifies # comments, the number of lines in a modified file will not be changed by this code
+
+    EXAMPLE:
+        >>> import rp
+
+        >>> #This function modifies your code - it adds comments next to debug_comment that show what it evaluated
+        >>> rp.debug_comment(rp.debug_comment)# --> <function debug_comment at 0x10717cee0>
+
+        >>> #If you align the comments like this it will respect the alignment upon updating
+        >>> rp.debug_comment(rp.debug_comment.__module__)# --> rp.experimental.debug_comment
+        >>> rp.debug_comment(rp)                         # --> <module 'rp' from '/opt/homebrew/lib/python3.10/site-packages/rp/__init__.py'>
+        >>> rp.debug_comment(__file__)                   # --> /Users/ryan/CleanCode/Sandbox/debugevaltest/evaltest.py
+        >>> rp.debug_comment(__name__)                   # --> __main__
+
+        >>> rp.debug_comment("What\nHappens\nWith\nMultiple\nLines")# --> 'What\nHappens\nWith\nMultiple\nLines'
+
+        >>> #The last evaluation will be the one that's kept
+        >>> for i in range(5):
+        >>> rp.debug_comment(i)   # --> 4
+        >>> rp.debug_comment(i**2)# --> 16
+
+        >>> rp.debug_comment(# --> I is 4
+        >>> "I is "+str(i)
+        >>> )   
+
+        >>> #This is what happens if you call it on multiple lines
+        >>> rp.debug_comment(# --> 127
+        >>> 123+i
+        >>> )   
+
+        >>> #If you give it a function that takes 0 args, it will try to evaluate it
+        >>> #This is good for if you want to disable debug_comments and disable all the evaluation inside them too to make your code faster
+        >>> rp.debug_comment(lambda: 22*22)# --> 484
+
+        >>> #Use cases:
+        >>> import torch
+        >>> t=torch.randn(10,100).cpu()
+        >>> rp.debug_comment(t.shape) # --> torch.Size([10, 100])
+        >>> rp.debug_comment(t.dtype) # --> torch.float32
+        >>> rp.debug_comment(t.min()) # --> tensor(-3.1440)
+        >>> rp.debug_comment(t.max()) # --> tensor(2.9910)
+        >>> rp.debug_comment(t.device)# --> cpu
+    """
+    # TODO: allow varargs
+    # Kwargs will be reserved for special purposes, like if we want to also record the date, or number of times the value changed or number of times we called debug comment etc etc
 
     # Get caller's frame
     frame = inspect.currentframe().f_back
@@ -33,9 +81,10 @@ def debug_comment(value):
         if is_zero_arg_callable(value):
             # If value is a zero-argument callable, call it
             result = value()
-        elif isinstance(value, str):
-            # Evaluate the string expression
-            result = eval(value, frame.f_globals, frame.f_locals)
+        ## Disabled eval-strings because I found I often feed it strings...this only makes sense if we give it a string literal, requiring complex introspection and parsing
+        # elif isinstance(value, str):
+        #     # Evaluate the string expression
+        #     result = eval(value, frame.f_globals, frame.f_locals)
         else:
             # For non-callable, non-string values, convert directly to string
             result = value
@@ -48,18 +97,20 @@ def debug_comment(value):
     except Exception as e:
         result = "Error: " + str(e)
 
-    _debug_eval_results.append((filename, lineno, result))
-    
-    # Return result to allow inline usage if desired
-
-    if '\n' in result: 
+    if '\n' in result or '\r' in result: 
         result=repr(result) #No multiline allowed! Has to fit in a single comment string
 
-    return result
+    _debug_eval_results.append((filename, lineno, result))
+    
+    #Let original value pass through
+    return value
 
 def format_debug_comment(result):
-    print(result)
+    # _debug_print(result) # This can get a bit spammy...
     return comment_prefix+str(result)
+
+def _debug_print(x):
+    rp.fansi_print('rp.debug_comment: '+str(x), 'green', 'underlined')
 
 comment_prefix = "# --> " #Make this regex-friendly. TODO: regex escape this
 
@@ -76,10 +127,11 @@ def _perform_debug_eval_substitutions():
         new_comment = format_debug_comment(result)
         current_line = lines[lineno - 1]
         
-        # Check if there's an existing debug comment and replace it
         #TODO: Do this with split_python_tokens instead and do more checks to make sure the source wasn't modified as it was running so that we're replacing the right line. AKA check to make sure this line in the file matches the line in the linecache, and starts with either rp.debug_comment or debug_comment. And check to make sure we only modify the last --> because --> might be in the value itself (unlikely edge case granted).
+
+        # Check if there's an existing debug comment and replace it
         if comment_prefix in current_line:
-            current_line = re.sub(comment_prefix + r'.+?(?=\n|$)', new_comment, current_line)
+            current_line = current_line[:current_line.rfind(comment_prefix)] + new_comment + '\n'
         else:
             current_line = current_line.rstrip() + new_comment + '\n'
         
@@ -90,10 +142,10 @@ def _perform_debug_eval_substitutions():
     for filename, lines in file_changes.items():
         with open(filename, 'w') as file:
             try:
-                rp.fansi_print("rp.debug_comment: writing to "+filename, 'green', 'underlined')
+                _debug_print("writing to "+filename)
                 file.writelines(lines)
             except Exception:
-                rp.fansi_print("rp.debug_comment: Failed to modify "+filename, 'green', 'underlined')
+                _debug_print("Failed to modify "+filename)
 
 # Example usage within the same file
 if __name__ == "__main__":
