be44d5f11b59073b5ffddfd6375f5f4a3c780b8b
Ryan Burgert (Retina) <ryancentralorg@gmail.com>
2025-02-04 00:28:11 -0500

    Main

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 import sys
 import rp
 
-scope = rp.r.__dict__
+default_scope = rp.r.__dict__
 
 def arg_eval(code):
-    if not rp.is_namespaceable(code) and rp.r._is_valid_exeval_python_syntax(code):
+    if not rp.is_namespaceable(code) and rp.r._is_valid_exeval_python_syntax(code) or code in default_scope:
         #Try to evaluate it. Include rp in the scope for easy access.
+        scope = dict(default_scope)
         value = rp.exeval(code, scope=scope)
     else:
         #Just treat it as a string
@@ -32,17 +33,35 @@ def pop_args():
         args.insert(0, value)
     return args
 
+help_text = """
+
+HELP:
+    Usage: rp <command> [<args>]
+
+    Available commands:
+      help    Show this help message
+      call    Call a function with arguments and keyword arguments
+              Format: rp call <funcname> arg1 arg2 arg3 --kwarg1 value1 --kwarg2 value2
+      exec    Execute Python code with optional variable assignments
+              Format: rp exec <code> --variable1 value1 --variable2 value2
+
+    If no command is provided, an interactive Python terminal (rp.pterm) is started.
+"""
+
 def main():
-    if len(sys.argv) > 2: 
+    if len(sys.argv) > 1: 
         if sys.argv[1] == "call":
             #Lets you call a function
             #rp call fansi_print "Hello World!" green bold
+
+            if not len(sys.argv) > 2:
+                raise RuntimeError("rp call: Please provide a funcname. Format: 'rp call <funcname> arg1 arg2 arg3 --kwarg1 value1 --kwarg2 value2'")
             
             func_name = sys.argv[2]
             kwargs = pop_kwargs()
             args   = pop_args()
 
-            scope = dict(scope)
+            scope = dict(default_scope)
             scope.update({"args__" : args, "kwargs__" : kwargs})
 
             #TODO: THE SCOPE SHOULD INCLUDE ALL IMPORTS FROM RP
@@ -51,17 +70,23 @@ def main():
         elif sys.argv[1] == "exec":
             # Import everything from your package at the top level
 
+            if not len(sys.argv) > 2:
+                raise RuntimeError("rp exec: Please provide code. Format: 'rp call <code> --variable1 value1 --variable2 value2'")
+
             # Allow kwargs: "rp exec 'print(x+y)' --x 123 --y 321"
             # Command is like:   rp exec print(x) --x 123
             # Or most minimally, like:   rp exec print(x) --x 123
             kwargs = pop_kwargs()
 
-            scope = dict(scope)
+            scope = dict(default_scope)
             scope.update(kwargs)
 
             code = ' '.join(sys.argv[2:])
 
             ans = rp.exeval(code, scope=scope)
+
+        else:
+            raise RuntimeError(help_text+"\nERROR: Supported rp commands: [exec, call] not "+sys.argv[1])
 
         #It's often useful to pipe the ans by printing it
         if ans is not None:
